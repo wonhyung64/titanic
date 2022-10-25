d7ebcd07493e9ad65070f9a693bb3c19c2d2c5fc
WonHyung Shin <wonhyung64@icloud.com>
2022-10-25 14:02:33 +0900

    feat - complete eda

--- run.py
+++ run.py
@@ -44,14 +44,35 @@ fig_dist, ax = plt.subplots(figsize=(10, 10))
 sns.histplot(data=train_df, x="Survived", stat="density", discrete=True, ax=ax)
 
 #%% eda
-fig_bar, axes = plt.subplots(nrows=3, ncols=2, figsize=(10, 10))
+train_df["Age_group"] = train_df["Age"].map(lambda x: int(x)%10 * 10 if not np.isnan(x) else -10)
+
+fig_bar, axes = plt.subplots(nrows=3, ncols=2, figsize=(10, 15))
 sns.barplot(data=train_df, x="Pclass", y="Survived", ax=axes[0][0])
 sns.barplot(data=train_df, x="Sex", y="Survived", ax=axes[0][1])
 sns.barplot(data=train_df, x="SibSp", y="Survived", ax=axes[1][0])
 sns.barplot(data=train_df, x="Parch", y="Survived", ax=axes[1][1])
 sns.barplot(data=train_df, x="Embarked", y="Survived", ax=axes[2][0])
+sns.barplot(data=train_df, x="Age_group", y="Survived", ax=axes[2][1])
+
+
+fig_count, axes = plt.subplots(nrows=3, ncols=2, figsize=(10, 15))
+sns.countplot(data=train_df, x="Pclass", hue="Survived", ax=axes[0][0])
+sns.countplot(data=train_df, x="Sex", hue="Survived", ax=axes[0][1])
+sns.countplot(data=train_df, x="SibSp", hue="Survived", ax=axes[1][0])
+sns.countplot(data=train_df, x="Parch", hue="Survived", ax=axes[1][1])
+sns.countplot(data=train_df, x="Embarked", hue="Survived", ax=axes[2][0])
+sns.countplot(data=train_df, x="Age_group", hue="Survived", ax=axes[2][1])
+
+
+fig_scatter, ax = plt.subplots(figsize=(10, 10))
+sns.scatterplot(data=train_df, x="Age", y="Fare", hue="Survived", ax=ax)
+
+train_df["Sex"] = train_df["Sex"].map(lambda x: {"male":0, "female":1}[x])
+train_df["Embarked"] = train_df["Embarked"].map(lambda x: {"S":0, "C":1, "Q":2}[x] if type(x)==str else -1)
+corr_df = train_df[~train_df["Embarked"].isna()].corr()
+
 
 fig_corr, ax = plt.subplots(figsize=(10, 10))
-sns.heatmap(train_df.corr(), annot=True, square=True)
+sns.heatmap(corr_df, annot=True, square=True)
 
 # %%
